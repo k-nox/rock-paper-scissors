3d8de681401abd14c7a3e107270d38823c10dec7
knox <lux.barker13@gmail.com>
2021-01-23 12:05:52 -0800

    Replace console messages with p elements
    Begin basic visual layout of game

--- main.js
+++ main.js
@@ -22,174 +22,87 @@ function computerPlay() {
   let result = getRandomInt();
   return convertRandomInt(result);
 }
-
-// obtains player answer and converts to match computer answer
-// function playerPlay() {
-//   let result = prompt('Rock, paper, or scissors?');
-
-//   const rock = /^r[0o][ck]{2}$/i;
-//   const paper = /^p[a@]p[e3]r$/i;
-//   const scissors = /^[s5z][ck]i[s5z]{2}[o0]r[s5z]$/i;
-
-//   result = result.replace(rock, 'rock');
-//   result = result.replace(paper, 'paper');
-//   result = result.replace(scissors, 'scissors');
-
-//   if (result === 'rock' || result === 'paper' || result === 'scissors') {
-//     return result;
-//   } else {
-//     return 'There must be an error somewhere. Please try again!';
-//   }
-// }
-
-// runs playerPlay and computerPlay and returns result of one round
-// let finalResult;
-
-// function playRound(playerSelection, computerSelection) {
-//   let playerAnswer = playerSelection();
-//   let computerAnswer = computerSelection();
-
-//   if (playerAnswer === computerAnswer) {
-//     finalResult = 'tie';
-//     return finalResult;
-//   } else if (playerAnswer === 'rock') {
-//     if (computerAnswer === 'paper') {
-//       finalResult = 'You lose! Paper beats rock.';
-//       return finalResult;
-//     } else {
-//       finalResult = 'You win! Rock smashes scissors.';
-//       return finalResult;
-//     }
-//   } else if (playerAnswer === 'paper') {
-//     if (computerAnswer === 'rock') {
-//       finalResult = 'You win! Paper beats rock.';
-//       return finalResult;
-//     } else {
-//       finalResult = 'You lose! Scissors cut paper.';
-//       return finalResult;
-//     }
-//   } else if (playerAnswer === 'scissors') {
-//     if (computerAnswer === 'paper') {
-//       finalResult = 'You win! Scissors cut paper.';
-//       return finalResult;
-//     } else {
-//       finalResult = 'You lose! Rock smashes scissors.';
-//       return finalResult;
-//     }
-//   } else {
-//     finalResult = 'There must be an error somewhere, please try again!';
-//     return finalResult;
-//   }
-// }
-
-// function game() {
-//   let computerScore = 0;
-//   let playerScore = 0;
-//   for (let i = 0; i < 5; i++) {
-//     playRound(playerPlay, computerPlay);
-//     switch (finalResult) {
-//       case 'tie':
-//         console.log(`Round Results: You tied! Let's keep going.
-//                         Computer score: ${computerScore}
-//                         Player Score: ${playerScore} `);
-//         break;
-//       case 'You lose! Paper beats rock.':
-//         computerScore++;
-//         console.log(`Round Results: ${finalResult}
-//                         Computer score: ${computerScore}
-//                         Player Score: ${playerScore} `);
-//         break;
-//       case 'You win! Rock smashes scissors.':
-//         playerScore++;
-//         console.log(`Round Results: ${finalResult}
-//                         Computer score: ${computerScore}
-//                         Player Score: ${playerScore} `);
-//         break;
-//       case 'You win! Paper beats rock.':
-//         playerScore++;
-//         console.log(`Round Results: ${finalResult}
-//                         Computer score: ${computerScore}
-//                         Player Score: ${playerScore} `);
-//         break;
-//       case 'You lose! Scissors cut paper.':
-//         computerScore++;
-//         console.log(`Round Results: ${finalResult}
-//                         Computer score: ${computerScore}
-//                         Player Score: ${playerScore} `);
-//         break;
-//       case 'You win! Scissors cut paper.':
-//         playerScore++;
-//         console.log(`Round Results: ${finalResult}
-//                         Computer score: ${computerScore}
-//                         Player Score: ${playerScore} `);
-//         break;
-//       case 'You lose! Rock smashes scissors.':
-//         computerScore++;
-//         console.log(`Round Results: ${finalResult}
-//                         Computer score: ${computerScore}
-//                         Player Score: ${playerScore} `);
-//         break;
-//       default:
-//         console.log(
-//           'Sorry, I think there is an error somewhere. Pleae try again!'
-//         );
-//     }
-//   }
-//   if (playerScore > computerScore) {
-//     console.log(
-//       `Congrats! You beat the computer ${playerScore} to ${computerScore}`
-//     );
-//   } else if (playerScore < computerScore) {
-//     console.log(
-//       `Oof... The computer beat you ${computerScore} to ${playerScore}!`
-//     );
-//   } else {
-//     console.log(`Looks like you tied this time! Wanna go again?`);
-//   }
-// }
+let playerScore = 0;
+let computerScore = 0;
 
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
+const resultsDiv = document.querySelector('.results');
 
-rock.addEventListener('click', playerPlay);
-paper.addEventListener('click', playerPlay);
-scissors.addEventListener('click', playerPlay);
+const playerScoreTracker = document.createElement('p');
+const computerScoreTracker = document.createElement('p');
+const roundMessage = document.createElement('p');
+const endMessage = document.createElement('p');
 
-function playerPlay(e) {
+rock.addEventListener('click', game);
+paper.addEventListener('click', game);
+scissors.addEventListener('click', game);
+
+//* plays game when button is clicked and keeps track of score
+function game(e) {
+  if (playerScore >= 5 || computerScore >= 5) {
+    //* resets scores to 0 if there has been a winner
+    playerScore = 0;
+    computerScore = 0;
+  }
   playRound(e.target.id, computerPlay());
+
+  playerScoreTracker.textContent = `Your score: ${playerScore}`;
+  resultsDiv.appendChild(playerScoreTracker);
+
+  computerScoreTracker.textContent = `Computer score: ${computerScore}`;
+  resultsDiv.appendChild(computerScoreTracker);
+  if (playerScore === 5) {
+    endMessage.textContent = `Game over! You beat the computer ${playerScore} to ${computerScore}!`;
+    resultsDiv.appendChild(endMessage);
+  }
+  if (computerScore === 5) {
+    endMessage.testContent = `How embarassing! The computer beat you ${computerScore} to ${playerScore}.`;
+    resultsDiv.appendChild(endMessage);
+  }
 }
 
+//* one round of gameplay with messages
 function playRound(playerSelection, computerSelection) {
-  const computerWin = 'computer win';
-  const playerWin = 'player win';
+  const computerWinMessage = `The computer wins this round - ${computerSelection} beats ${playerSelection}!`;
+  const playerWinMessage = `You won this round - ${playerSelection} beats ${computerSelection}!`;
+  const tieMessage = 'This round was a tie.';
+  const tie = 'tie';
 
   if (playerSelection === computerSelection) {
-    console.log('tie');
-    return 'tie';
+    roundMessage.textContent = tieMessage;
+    resultsDiv.appendChild(roundMessage);
+    return tie;
   } else if (playerSelection === 'rock') {
     if (computerSelection === 'paper') {
-      console.log(computerWin);
-      return computerWin;
+      roundMessage.textContent = computerWinMessage;
+      resultsDiv.appendChild(roundMessage);
+      return computerScore++;
     } else {
-      console.log(playerWin);
-      return playerWin;
+      roundMessage.textContent = playerWinMessage;
+      resultsDiv.appendChild(roundMessage);
+      return playerScore++;
     }
   } else if (playerSelection === 'paper') {
     if (computerSelection === 'rock') {
-      console.log(playerWin);
-      return playerWin;
+      roundMessage.textContent = playerWinMessage;
+      resultsDiv.appendChild(roundMessage);
+      return playerScore++;
     } else {
-      console.log(computerWin);
-      return computerWin;
+      roundMessage.textContent = computerWinMessage;
+      resultsDiv.appendChild(roundMessage);
+      return computerScore++;
     }
   } else if (playerSelection === 'scissors') {
     if (computerSelection === 'rock') {
-      console.log(computerWin);
-      return computerWin;
+      roundMessage.textContent = computerWinMessage;
+      resultsDiv.appendChild(roundMessage);
+      return computerScore++;
     } else {
-      console.log(playerWin);
-      return playerWin;
+      roundMessage.textContent = playerWinMessage;
+      resultsDiv.appendChild(roundMessage);
+      return playerScore++;
     }
   } else {
     return 'ERROR';
