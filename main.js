04a276e74fa771d2caa930624556feefe04c6eb6
knox <lux.barker13@gmail.com>
2021-01-28 16:54:19 -0800

    Set up prettier and eslint

--- main.js
+++ main.js
@@ -5,21 +5,26 @@ function getRandomInt() {
 
 //* converts number between 0-2 into rock, paper, or scissors
 function convertRandomInt(num) {
+  let answer = '';
   switch (num) {
     case 0:
-      return 'rock';
+      answer = 'rock';
       break;
     case 1:
-      return 'paper';
+      answer = 'paper';
       break;
     case 2:
-      return 'scissors';
+      answer = 'scissors';
+      break;
+    default:
+      answer = 'ERROR';
   }
+  return answer;
 }
 
 //* calls getRandomInt and converts result using convertRandomInt
 function computerPlay() {
-  let result = getRandomInt();
+  const result = getRandomInt();
   return convertRandomInt(result);
 }
 let playerScore = 0;
@@ -35,45 +40,15 @@ const computerScoreTracker = document.createElement('p');
 const roundMessage = document.createElement('p');
 const endMessage = document.createElement('p');
 
-rock.addEventListener('click', game);
-paper.addEventListener('click', game);
-scissors.addEventListener('click', game);
-
-//* plays game when button is clicked and keeps track of score
-function game(e) {
-  if (playerScore >= 5 || computerScore >= 5) {
-    //* resets scores to 0 if there has been a winner
-    playerScore = 0;
-    computerScore = 0;
-  }
-  playRound(e.target.id, computerPlay());
-
-  playerScoreTracker.textContent = `Your score: ${playerScore}`;
-  resultsDiv.appendChild(playerScoreTracker);
-
-  computerScoreTracker.textContent = `Computer score: ${computerScore}`;
-  resultsDiv.appendChild(computerScoreTracker);
-  if (playerScore === 5) {
-    endMessage.textContent = `Game over! You beat the computer ${playerScore} to ${computerScore}!`;
-    resultsDiv.appendChild(endMessage);
-  }
-  if (computerScore === 5) {
-    endMessage.testContent = `How embarassing! The computer beat you ${computerScore} to ${playerScore}.`;
-    resultsDiv.appendChild(endMessage);
-  }
-}
-
 //* one round of gameplay with messages
 function playRound(playerSelection, computerSelection) {
   const computerWinMessage = `The computer wins this round - ${computerSelection} beats ${playerSelection}!`;
   const playerWinMessage = `You won this round - ${playerSelection} beats ${computerSelection}!`;
   const tieMessage = 'This round was a tie.';
-  const tie = 'tie';
 
   if (playerSelection === computerSelection) {
     roundMessage.textContent = tieMessage;
-    resultsDiv.appendChild(roundMessage);
-    return tie;
+    return resultsDiv.appendChild(roundMessage);
   } else if (playerSelection === 'rock') {
     if (computerSelection === 'paper') {
       roundMessage.textContent = computerWinMessage;
@@ -108,3 +83,31 @@ function playRound(playerSelection, computerSelection) {
     return 'ERROR';
   }
 }
+
+//* plays game when button is clicked and keeps track of score
+function game(e) {
+  if (playerScore >= 5 || computerScore >= 5) {
+    //* resets scores to 0 if there has been a winner
+    playerScore = 0;
+    computerScore = 0;
+  }
+  playRound(e.target.id, computerPlay());
+
+  playerScoreTracker.textContent = `Your score: ${playerScore}`;
+  resultsDiv.appendChild(playerScoreTracker);
+
+  computerScoreTracker.textContent = `Computer score: ${computerScore}`;
+  resultsDiv.appendChild(computerScoreTracker);
+  if (playerScore === 5) {
+    endMessage.textContent = `Game over! You beat the computer ${playerScore} to ${computerScore}!`;
+    resultsDiv.appendChild(endMessage);
+  }
+  if (computerScore === 5) {
+    endMessage.textContent = `How embarassing! The computer beat you ${computerScore} to ${playerScore}.`;
+    resultsDiv.appendChild(endMessage);
+  }
+}
+
+rock.addEventListener('click', game);
+paper.addEventListener('click', game);
+scissors.addEventListener('click', game);
