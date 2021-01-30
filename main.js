674efcc61592b842b50b2f2e6d4311f4bb83cf2a
knox <lux.barker13@gmail.com>
2021-01-29 20:16:55 -0800

    Finalize game
    Create different round messages to add interest and variety
    Finalize animations and transitions

--- main.js
+++ main.js
@@ -34,32 +34,226 @@ const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
 const resultsDiv = document.querySelector('.results');
+const startDiv = document.querySelector('.start');
+const wrapper = document.querySelector('.wrapper');
 
 const playerScoreTracker = document.createElement('p');
 const computerScoreTracker = document.createElement('p');
 const roundMessage = document.createElement('p');
+const endDiv = document.createElement('div');
 const endMessage = document.createElement('p');
 const startOverMessage = document.createElement('p');
 
-playerScoreTracker.classList.add('generated', 'hidden');
-computerScoreTracker.classList.add('generated', 'hidden');
-roundMessage.classList.add('generated', 'hidden');
-endMessage.classList.add('generated', 'hidden');
-startOverMessage.classList.add('generated', 'hidden');
+playerScoreTracker.classList.add('start', 'hidden');
+computerScoreTracker.classList.add('start', 'hidden');
+roundMessage.classList.add('start', 'hidden');
+endDiv.classList.add('start', 'hidden');
+// endMessage.classList.add('start', 'hidden');
+// startOverMessage.classList.add('start', 'hidden');
 
 startOverMessage.textContent = 'To start over, just pick a new weapon!';
 
 resultsDiv.appendChild(roundMessage);
 resultsDiv.appendChild(playerScoreTracker);
 resultsDiv.appendChild(computerScoreTracker);
-resultsDiv.appendChild(endMessage);
-resultsDiv.appendChild(startOverMessage);
+resultsDiv.appendChild(endDiv);
+endDiv.appendChild(endMessage);
+endDiv.appendChild(startOverMessage);
+// resultsDiv.appendChild(endMessage);
+// resultsDiv.appendChild(startOverMessage);
+
+//* creates and returns round messages with random number generator
+function createRoundMessage(winner, weapon, num) {
+  let message;
+
+  //* if the computer wins this round:
+  if (winner === 'computer') {
+    //* if the computer picks rock
+    if (weapon === 'rock') {
+      switch (num) {
+        case 0:
+          message =
+            'Scissors are no match against a rock! The computer beat you this time.';
+          break;
+        case 1:
+          message = 'The computer wins this round! Rock smashes scissors.';
+          break;
+        case 2:
+          message = "What a shame! The computer's rock broke your scissors.";
+          break;
+        default:
+          message = 'ERROR';
+      }
+
+      //* if the computer picks paper:
+    } else if (weapon === 'paper') {
+      switch (num) {
+        case 0:
+          message =
+            "Your rock is no match for the machine's paper! You lost this round.";
+          break;
+        case 1:
+          message = 'The computer wins this round! Paper hides rock.';
+          break;
+        case 2:
+          message =
+            'Did you really think a rock would defeat paper? The computer takes this round!';
+          break;
+        default:
+          message = 'ERROR';
+      }
+
+      //* if the computer picks scissors:
+    } else if (weapon === 'scissors') {
+      switch (num) {
+        case 0:
+          message =
+            "Oof! The computer's scissors slice right through your paper.";
+          break;
+        case 1:
+          message = 'The computer wins this round! Scissors cut paper.';
+          break;
+        case 2:
+          message =
+            "You've brought shame on that nice paper. The computer shreded it with its scissors!";
+          break;
+        default:
+          message = 'ERROR';
+      }
+    }
+
+    //* if player is the winner:
+  } else if (winner === 'player') {
+    //* if the player picks rock
+    if (weapon === 'rock') {
+      switch (num) {
+        case 0:
+          message =
+            'How did you do it?! You won this round - rock crushes scissors!';
+          break;
+        case 1:
+          message = "You win this round! Rock smashes the computer's scissors.";
+          break;
+        case 2:
+          message =
+            "You really know your stuff! Your rock is way more dangerous than the computer's scissors.";
+          break;
+        default:
+          message = 'ERROR';
+      }
+
+      //* if the player picks paper:
+    } else if (weapon === 'paper') {
+      switch (num) {
+        case 0:
+          message =
+            "You successfully hid the computer's rock with your paper. It's nowhere to be found!";
+          break;
+        case 1:
+          message = 'You win this round! Paper covers rock.';
+          break;
+        case 2:
+          message =
+            "I can't believe the computer didn't think this through...your paper beats the computer's rock easily.";
+          break;
+        default:
+          message = 'ERROR';
+      }
+
+      //* if the player picks scissors:
+    } else if (weapon === 'scissors') {
+      switch (num) {
+        case 0:
+          message =
+            "Have you no mercy for paper? Your scissors shred the computer's paper to smithereens!";
+          break;
+        case 1:
+          message = 'You win this round! Scissors cut paper.';
+          break;
+        case 2:
+          message =
+            'You know, the computer told me it saved up a lot for that nice paper, and you just sliced it with your scissors like it meant nothing.';
+          break;
+        default:
+          message = 'ERROR';
+      }
+    }
+
+    //* if there is a tie
+  } else if (winner === 'tie') {
+    //* if both pick rock
+    if (weapon === 'rock') {
+      switch (num) {
+        case 0:
+          message = 'Well this is awkward. You both picked rock! Try again?';
+          break;
+        case 1:
+          message = 'This round is a tie! Rocks all around.';
+          break;
+        case 2:
+          message =
+            'Nah, rocks are boring. Pick something else because this round was tie.';
+          break;
+        default:
+          message = 'ERROR';
+      }
+
+      //* if both pick paper:
+    } else if (weapon === 'paper') {
+      switch (num) {
+        case 0:
+          message =
+            "Did you give me an office job? I'm drowing in paper here! You tied, so please try again.";
+          break;
+        case 1:
+          message = 'This round is a tie! Papers are flying everywhere.';
+          break;
+        case 2:
+          message =
+            "I have enough paperwork on my own, I don't need both you and the computer giving me more!";
+          break;
+        default:
+          message = 'ERROR';
+      }
+
+      //* if both pick scissors:
+    } else if (weapon === 'scissors') {
+      switch (num) {
+        case 0:
+          message =
+            "Between you and the computer, I'm feeling a little threatened by all the scissors. Maybe pick something else?";
+          break;
+        case 1:
+          message =
+            'This round is a tie! It appears you both have an affinity for scissors.';
+          break;
+        case 2:
+          message =
+            'I get it, I get it. Sharp pointy things are fun! But you both picked the same thing. Try again?';
+          break;
+        default:
+          message = 'ERROR';
+      }
+    }
+  } else {
+    message = 'ERROR';
+  }
+  return message;
+}
 
 //* one round of gameplay with messages
 function playRound(playerSelection, computerSelection) {
-  const computerWinMessage = `The computer wins this round - ${computerSelection} beats ${playerSelection}!`;
-  const playerWinMessage = `You won this round - ${playerSelection} beats ${computerSelection}!`;
-  const tieMessage = 'This round was a tie.';
+  const computerWinMessage = createRoundMessage(
+    'computer',
+    computerSelection,
+    getRandomInt()
+  );
+  const playerWinMessage = createRoundMessage(
+    'player',
+    playerSelection,
+    getRandomInt()
+  );
+  const tieMessage = createRoundMessage('tie', playerSelection, getRandomInt());
 
   if (playerSelection === computerSelection) {
     roundMessage.textContent = tieMessage;
@@ -88,19 +282,27 @@ function playRound(playerSelection, computerSelection) {
       playerScore++;
     }
   }
-  roundMessage.classList.remove('hidden');
+  if (roundMessage.classList.contains('hidden')) {
+    roundMessage.classList.remove('hidden');
+  }
 }
 
 //* plays game when button is clicked and keeps track of score
 function game(e) {
+  if (!startDiv.classList.contains('hidden')) {
+    startDiv.classList.add('hidden');
+    wrapper.classList.add('up');
+  }
   if (playerScore >= 5 || computerScore >= 5) {
     //* resets scores to 0 if there has been a winner
     playerScore = 0;
     computerScore = 0;
-    endMessage.classList.add('hidden');
-    endMessage.classList.remove('end');
-    startOverMessage.classList.add('hidden');
-    startOverMessage.classList.remove('end');
+    endDiv.classList.add('hidden');
+    endDiv.classList.remove('end');
+    // endMessage.classList.add('hidden');
+    // endMessage.classList.remove('end');
+    // startOverMessage.classList.add('hidden');
+    // startOverMessage.classList.remove('end');
   }
   playRound(e.target.id, computerPlay());
 
@@ -113,10 +315,12 @@ function game(e) {
   if (playerScore === 5 || computerScore === 5) {
     playerScoreTracker.classList.add('hidden');
     computerScoreTracker.classList.add('hidden');
-    endMessage.classList.remove('hidden');
-    endMessage.classList.add('end');
-    startOverMessage.classList.remove('hidden');
-    startOverMessage.classList.add('end');
+    endDiv.classList.remove('hidden');
+    endDiv.classList.add('end');
+    // endMessage.classList.remove('hidden');
+    // endMessage.classList.add('end');
+    // startOverMessage.classList.remove('hidden');
+    // startOverMessage.classList.add('end');
 
     if (playerScore === 5) {
       endMessage.textContent = `Game over! You beat the computer ${playerScore} to ${computerScore}!`;
